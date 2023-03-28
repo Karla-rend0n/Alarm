ba240eb90c008ed52e2f320c4215102a3bbfeb76
Karla-rend0n <lupita1029384756@gmail.com>
2023-03-28 08:13:07 -0600

    Update App.js
    Proyecto Base

--- App.js
+++ App.js
@@ -15,71 +15,39 @@ import {
 import NativeBaseIcon from "./components/NativeBaseIcon";
 import { Platform } from "react-native";
 
-// Define the config
-const config = {
-  useSystemColorMode: false,
-  initialColorMode: "dark",
-};
 
-// extend the theme
-export const theme = extendTheme({ config });
 
 export default function App() {
+  const theme = extendTheme({
+    colors: {
+      // Add new color
+      primary: {
+        50: '#FFFFFF', // Blanco
+        100: '#DDDAE0', // Gris de input 100%
+        200: '#807E82',// gris de los botones fuerte 100%
+        300: '#D9D9D9', //botones de alarma y enmergencia 
+        400: '#',
+        500: '#C1591F',//Neranja
+        600: '#9B1010',//Rojo Emergencia
+        700: '#DDDAE0',//gris de los botones claro 70%
+        800: '#1B396A',// Azul
+        900: '#000000',//Negro
+      },
+      // Redefining only one shade, rest of the color will remain same.
+      amber: {
+        400: '#d97706',
+      },
+    },
+    config: {
+      // Changing initialColorMode to 'dark'
+      initialColorMode: 'dark',
+    },
+  });
   return (
-    <NativeBaseProvider>
-      <Center
-        _dark={{ bg: "blueGray.900" }}
-        _light={{ bg: "blueGray.50" }}
-        px={4}
-        flex={1}
-      >
-        <VStack space={5} alignItems="center">
-          <NativeBaseIcon />
-          <Heading size="lg">Welcome to NativeBase</Heading>
-          <HStack space={2} alignItems="center">
-            <Text>Edit</Text>
-            <Box
-              _web={{
-                _text: {
-                  fontFamily: "monospace",
-                  fontSize: "sm",
-                },
-              }}
-              px={2}
-              py={1}
-              _dark={{ bg: "blueGray.800" }}
-              _light={{ bg: "blueGray.200" }}
-            >
-              App.js
-            </Box>
-            <Text>and save to reload.</Text>
-          </HStack>
-          <Link href="https://docs.nativebase.io" isExternal>
-            <Text color="primary.500" underline fontSize={"xl"}>
-              Learn NativeBase
-            </Text>
-          </Link>
-          <ToggleDarkMode />
-        </VStack>
+    <NativeBaseProvider theme={theme}>
+      <Center alignItems="center">
+        
       </Center>
     </NativeBaseProvider>
-  );
-}
-
-// Color Switch Component
-function ToggleDarkMode() {
-  const { colorMode, toggleColorMode } = useColorMode();
-  return (
-    <HStack space={2} alignItems="center">
-      <Text>Dark</Text>
-      <Switch
-        isChecked={colorMode === "light"}
-        onToggle={toggleColorMode}
-        aria-label={
-          colorMode === "light" ? "switch to dark mode" : "switch to light mode"
-        }
-      />
-      <Text>Light</Text>
-    </HStack>
   );
 }
