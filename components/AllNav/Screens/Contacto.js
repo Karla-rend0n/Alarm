d1842eed91bdc9dadf47ee5f9928b96f0adb513d
JosueAbraham <rodriguez.razo.josue@gmail.com>
2023-04-11 07:56:36 -0600

    Create Contacto.js
    11/04/23 Contacto

---
+++ components/AllNav/Screens/Contacto.js
@@ -0,0 +1,22 @@
+import React from "react";
+import {Center, Box, Heading} from "native-base"
+
+export default function Contacto(){
+    return (
+        <Center w="100%" h="100%" bg="primary.800">
+            <Box safeArea p="1" py="8" w="100%" maxW="350">
+            <Heading size="xl" fontWeight="800" color="black" mt="-12" _dark={{
+                color: "primary.500"
+              }}>
+                Inicio
+            </Heading>
+    
+    
+    
+            </Box>
+        </Center>
+        
+    
+            );
+        
+    }
