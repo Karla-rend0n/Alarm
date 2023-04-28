e511357a49834b3d963b3b6a1c05889c9da0a08c
JosueAbraham <rodriguez.razo.josue@gmail.com>
2023-04-28 11:57:49 -0600

    Update MainNav.js

--- components/AllNav/MainNav.js
+++ components/AllNav/MainNav.js
@@ -1,15 +1,71 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
 
 //Screens
 import Inicio from "../AllNav/Screens/Inicio";
+
+
 import Contacto from "../AllNav/Screens/Contacto";
+
+
+
 import Perfil from "../AllNav/Screens/Perfil";
+import Perfil_Editar from "./Screens/Perfil_Editar";
+import Dirección from "./Screens/Dirección";
+import Dirección_Editar from "./Screens/Dirección_Editar";
+
+
 
 import { FontAwesome } from '@expo/vector-icons'; 
 import { SimpleLineIcons } from '@expo/vector-icons'; 
 import { Ionicons } from '@expo/vector-icons'; 
+
+
+
+//Para la navegación
+const HomeStackNavigator = createNativeStackNavigator();
+
+function MyStack(){
+    return(
+        <HomeStackNavigator.Navigator>
+        <HomeStackNavigator.Screen
+        name = "Perfil"
+        component={Perfil}
+        options={{
+            headerShown:false,
+        }}
+        />
+        
+        <HomeStackNavigator.Screen
+        name = "PantEditar"
+        component={Perfil_Editar}
+        options={{
+            headerBackTitleVisible:false,
+        }}
+        />
+        
+        <HomeStackNavigator.Screen
+        name = "PantDirección"
+        component={Dirección}
+        options={{
+            headerBackTitleVisible:false,
+        }}
+        />
+        
+        <HomeStackNavigator.Screen
+        name = "PantDireEditar"
+        component={Dirección_Editar}
+        options={{
+            headerBackTitleVisible:false,
+        }}
+        />
+        </HomeStackNavigator.Navigator>
+
+    )
+}
 
 
 //Guardar createBottom
@@ -52,7 +108,7 @@ function MyTabs() {
 
             <Tab.Screen 
             name="Perfil" 
-            component={Perfil} 
+            component={MyStack} 
             options={{
                 tabBarIcon:({color}) => (
                     <Ionicons name="person-circle-sharp" size={29} color={color}  />
