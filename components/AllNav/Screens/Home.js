caedf0874aa9fe2d0ebb4e32549bdb499e7fdd44
JosueAbraham <rodriguez.razo.josue@gmail.com>
2023-09-07 20:29:58 -0600

    Cambio de diseño y botones al centro
    Cambio de diseño y botones al centro

--- components/AllNav/Screens/Home.js
+++ components/AllNav/Screens/Home.js
@@ -1,16 +1,22 @@
 import React from "react";
 import { Center, Box, Heading, Button, Text, TouchableOpacity, ScrollView, useState, Flex, Stack, VStack } from "native-base"
 import { Linking } from "react-native"
+import { Dimensions } from "react-native";
+
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+
 
 
 //export default function Home() {
-export default function Home({route}) {
+export default function Home({ route }) {
 
     const showMessage = () => Alert.alert('Button clicked !');
-    const {data_profile} = route.params
+    const { data_profile } = route.params
     const [estadoBoton, setEstadoBoton] = React.useState('Apagado');
     const [tiempoInicioPresionado, setTiempoInicioPresionado] = React.useState(0);
-    
+
     const handlePressIn = () => {
         setTiempoInicioPresionado(Date.now());
     };
@@ -19,7 +25,7 @@ export default function Home({route}) {
         console.log('Status', status)
         let profile_contact = data_profile[0].profile_contact
         let contact_phone = '+52' + profile_contact[0].phone
-        let contact_name =  profile_contact[0].name + ' ' + profile_contact[0].last_name
+        let contact_name = profile_contact[0].name + ' ' + profile_contact[0].last_name
 
         let profile_name = data_profile[0].name + ' ' + data_profile[0].last_name
         console.log('data_profile', profile_name)
@@ -90,38 +96,35 @@ export default function Home({route}) {
 
 
     return (
-        <ScrollView w="100%" h="100%">
-
-            <Center w="100%" h="154%" bg={{
+        <ScrollView flex={1} contentContainerStyle={{ flexGrow: 1 }}>
+            <Center flex={1} bg={{ // Utilizamos flex para ocupar todo el espacio disponible
                 linearGradient: {
                     colors: ['primary.400', 'primary.800'],
                     start: [1, 0],
                     end: [0, 0]
-
                 }
             }}>
-                <Box safeArea p="2" py="8" w="100%" h="94%" maxW="350px" >
+                <Box safeArea p="2" py="8" w="100%" maxWidth={windowWidth * 0.8} >
 
                     <Heading size="xl" color="Black" _dark={{
                         color: "primary.50",
                         fontWeight: 'bold'
-                    }} >
+                    }}>
                         Botón de emergencia
                     </Heading>
 
+                    <VStack space={windowHeight * 0.05} mt={windowHeight * 0.05}>
+
+
+                        <Heading mt="3" color="primary.50" fontWeight='medium' size='md'>
+                            Si vez algo sospechoso presiona el botón.
+                        </Heading>
 
 
 
-                    <Heading mt="10" color="primary.50" fontWeight='medium' size='md' >
-                        Si vez algo sospechoso presiona el botón.
-                    </Heading>
-
-                    <VStack space={10} mt={10}>
-
-
-                        <Button mt='3' rounded borderRadius="200" width="200" height="200"  bgColor="primary.300" borderWidth="3" borderColor="primary.900" onPressIn={handlePressIn} onPressOut={handlePressOut}>
-                            <Button rounded borderRadius="170" width="170" height="170" bgColor="primary.500" borderWidth="4" borderColor="primary.1000" onPressIn={handlePressIn} onPressOut={handlePressOut}>
-                                <Text>{estadoBoton}</Text>
+                        <Button mt='3' rounded borderRadius="200" width="200" height="200" bgColor="primary.300" borderWidth="3" borderColor="primary.900" onPressIn={handlePressIn} onPressOut={handlePressOut} alignItems={'center'} alignSelf={'center'}>
+                            <Button rounded borderRadius="170" width="170" height="170" bgColor="primary.500" borderWidth="4" borderColor="primary.1000" onPressIn={handlePressIn} onPressOut={handlePressOut} alignItems={'center'} alignSelf={'center'}>
+                                <Text alignItems={'center'} alignSelf={'center'}>{estadoBoton}</Text>
 
                                 <Text fontWeight="700">Alarma</Text>
 
@@ -130,16 +133,16 @@ export default function Home({route}) {
 
                         <Flex direction="row" alignItems="center" >
 
-                            <Heading mt="10" color="primary.50" fontWeight='medium' size='md' >
+                            <Heading mt={windowHeight * 0.03} color="primary.50" fontWeight='medium' size='md' >
                                 En caso de emergencia presione el botón.
                             </Heading>
                         </Flex>
 
-                        <Button mt='3' rounded borderRadius="200" width="200" height="200"  bgColor="primary.300" borderWidth="3" borderColor="primary.900" onPressIn={handlePressInicio} onPressOut={handlePressApagado}>
-                            <Button rounded borderRadius="170" width="170" height="170"  bgColor="primary.600" borderWidth="4" borderColor="primary.1001" onPressIn={handlePressInicio} onPressOut={handlePressApagado}>
-                                <Text>{estadoBotonE}</Text>
+                        <Button mt='3' rounded borderRadius="200" width="200" height="200" bgColor="primary.300" borderWidth="3" borderColor="primary.900" onPressIn={handlePressInicio} onPressOut={handlePressApagado} alignItems={'center'} alignSelf={'center'}>
+                            <Button rounded borderRadius="170" width="170" height="170" bgColor="primary.600" borderWidth="4" borderColor="primary.1001" onPressIn={handlePressInicio} onPressOut={handlePressApagado} alignItems={'center'} alignSelf={'center'}>
+                                <Text alignItems={'center'} alignSelf={'center'}>{estadoBotonE}</Text>
 
-                                <Text  fontWeight="700">Emergencia</Text>
+                                <Text fontWeight="700">Emergencia</Text>
 
                             </Button>
 
